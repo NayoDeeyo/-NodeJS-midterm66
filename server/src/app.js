ed88d807c633a5bdde79bb1599b7968edd703848
NayoDeeyo <call0979758170@gmail.com>
2023-08-23 12:41:50 +0700

    'MidtermExam66'

--- server/src/app.js
+++ server/src/app.js
@@ -6,6 +6,8 @@ const app = express()
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 
+require('./routes')(app)
+
 app.get('/status', function (req, res){
     res.send('hello nodejs server')
 })
@@ -13,31 +15,6 @@ app.get('/status', function (req, res){
 app.get('/hello/:person', function (req,res) {
     console.log('hello -' + req.params.person)
     res.send('say hello with ' + req.params.person)
-})
-
-//get user by id
-app.get('/user/:userId', function (req, res) {
-    res.send('ดูข้อมูลผู้ใช้งาน : ' + req.params.userId)
-})
-
-//get all user
-app.get('/users', function (req, res) {
-    res.send('เรียกดูข้อมูลผู้ใช้งานทั้งหมด')
-})
-
-// create user
-app.post('/users/', function (req, res) {
-    res.send('ทำการสร้างผู้ใช้งาน' + JSON.stringify(req.body))
-})
-
-// edit user
-app.put('/user/:userId', function (req, res) {
-    res.send('ทำการแก้ไขผู้ใช้งาน' + req.params.userId + ' : ' + JSON.stringify(req.body))
-})
-
-// delete user
-app.delete('/user/:userId', function (req, res) {
-    res.send('ทำการลบผู้ใช้งาน' + req.params.userId + ' : ' + JSON.stringify(req.body))
 })
 
 let port = 8081
